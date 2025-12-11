4dd15339dee0eb36fbbb0c6affe12d4b1b9abb63
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-12-11 10:14:37 +0100

    fixe after IA explication pull request

--- src/api/goods.ts
+++ src/api/goods.ts
@@ -4,15 +4,30 @@ import { Good } from '../types/Good';
 const API_URL = `https://mate-academy.github.io/react_dynamic-list-of-goods/goods.json`;
 
 export function getAll(): Promise<Good[]> {
-  return fetch(API_URL).then(response => response.json());
+  return fetch(API_URL)
+    .then(response => response.json())
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching all goods', error);
+    });
 }
 
 export const get5First = () => {
-  return getAll().then(goods =>
-    goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
-  ); // sort and get the first 5
+  return getAll()
+    .then(goods =>
+      goods.sort((a, b) => a.name.localeCompare(b.name)).slice(0, 5),
+    )
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching 5 goods', error);
+    }); // sort and get the first 5
 };
 
 export const getRedGoods = () => {
-  return getAll().then(goods => goods.filter(red => red.color === 'red')); // get only red
+  return getAll()
+    .then(goods => goods.filter(red => red.color === 'red'))
+    .catch(error => {
+      // eslint-disable-next-line no-console
+      console.error('Error fetching red Goods', error);
+    }); // get only red
 };
