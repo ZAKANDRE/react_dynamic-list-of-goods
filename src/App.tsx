232ff963ccd6c343136ba93e0fadbbbce6b66e2b
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-12-11 09:44:28 +0100

    saved before pull up

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,75 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
-
-// import { getAll, get5First, getRed } from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import type { Good } from './types/Good';
 // or
 // import * as goodsAPI from './api/goods';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+export const App: React.FC = () => {
+  const [goodsAll, setGoodsAll] = useState<Good[]>([]);
+  const [goodsFive, setGoodsFive] = useState<Good[]>([]);
+  const [redGoods, setRedGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const [switchGoods, setSwitchGoods] = useState<string>('');
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  useEffect(() => {
+    getAll()
+    .then((goodsFromServer)=>{
+      setGoodsAll(goodsFromServer);
+    })
+    
+    get5First().then((fiveGoodsFromServer) => {
+      setGoodsFive(fiveGoodsFromServer);
+    });
+
+    getRedGoods().then((redsFromServer) => {
+      setRedGoods(redsFromServer)
+    })
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" 
+              data-cy="all-button"
+              onClick={() => setSwitchGoods('all')}
+      >
+        Load all goods
+      </button>
+
+      <button type="button"
+              data-cy="first-five-button"
+              onClick={() => setSwitchGoods('five')}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" 
+              data-cy="red-button"
+              onClick={()=> setSwitchGoods('red')}
+      >
+        Load red goods
+      </button>
+      {switchGoods === 'all' 
+        ? (
+          <GoodsList goods={goodsAll} />
+        ) : null
+      }
+      {switchGoods === 'five' 
+        ? (
+          <GoodsList goods={goodsFive} />
+        ) : null
+      }
+      {switchGoods === 'red'
+        ? (
+          <GoodsList goods={redGoods}/>
+        ): null
+
+      }
+    </div>
+  );
+}
